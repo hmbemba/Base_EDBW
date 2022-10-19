f5018208695c6ab787d77833f07fd14cddf5efda
hmbemba <hmbemba@gmail.com>
2022-10-18 21:28:39 -0400

    arg

--- db/Models/MyModel.py
+++ db/Models/MyModel.py
@@ -2,11 +2,16 @@ import edgedb
 from db.edbw.EdgeDBModel import EdgeDBModel
 from db.edbw.Properties import Type
 import pprint
+import os
+from dotenv import load_dotenv
+
 pp = pprint.PrettyPrinter(indent=4)
+load_dotenv()
 
-client = edgedb.create_client()
+localClient = edgedb.create_client()
+remoteClient = edgedb.create_client(dsn=os.eviron['DSN'], tls_security='insecure')
 
-InvoicesModel = EdgeDBModel(modelName='Invoices', client=client)
+InvoicesModel = EdgeDBModel(modelName='Invoices', client=localClient )#remoteClient)
 InvoicesModel.addProperty(_propertyName = 'three_word_name', _propertyType = Type.str, _req = True)
 
 #InvoicesModel.getByProperty(printStr=True, propName='three_word_name', propType=Type.str, _three_word_name='TameHolographcScallop')
